6e93d6315716361cf083a588fc9b8db23039f27a
lac <laccat@qq.com>
2019-09-17 16:57:13 +0800

    1

---
+++ AudioPlayer/js/index.js
@@ -0,0 +1,15 @@
+
+
+// 用ajax 获取数据
+// var xml = new XMLHttpRequest()
+// xml.open('get', 'http://localhost:8080/music.json', ture)
+// xml.onload = function () {
+//   window.musicList = JSON.parse(xml.responseText)
+// }
+
+// f(){
+//   window.musicList = JSON.parse(xml.responseText)
+// }
+
+// xml.send()
+// musicList
