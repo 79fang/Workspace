559b236e87d009b0f22ad30704864a8face4530a
79fang <laccat@qq.com>
2021-11-06 22:53:40 +0800

    v

--- vue/_19单文件组件/vue_test01/src/App.vue
+++ vue/_19单文件组件/vue_test01/src/App.vue
@@ -4,20 +4,23 @@
     <!-- 通过父组件给子组件传递函数类型的props实现:子给父传递数据 -->
     <School :getSchoolName="getSchoolName" />
 
-    <!-- 通过父组件给子组件绑定一个自定义事件实现:子给父传递数据 -->
-    <!-- <Student @atguigu="getStudentName" /> -->
+    <!-- 通过父组件给子组件绑定一个自定义事件实现:子给父传递数据(第一种写法,使用@或v-on) -->
+    <!-- //once 触发一次 -->
+    <Student @atguigu.once="getStudentName" />  
 
-    <Student ref="student" />
+    <!-- 通过父组件给子组件绑定一个自定义事件实现:子给父传递数据(第二种写法,使用ref) -->
+    <!-- <Student ref="student" /> -->
   </div>
 </template>
 
 <script>
+ import Student from './components/Student'
   import School from './components/School'
-  import Student from './components/Student.vue'
+ 
 
 export default {
   name:'App',
-  components:{School,StudentStudent},
+  components:{School,Student},
   data(){
     return {
       msg: 'hello!!'
@@ -27,10 +30,16 @@ export default {
     getSchoolName(name){
       console.log('App 收到了 SchoolName:',name)
     },
-   getStudentName(name){
-      console.log('App get StudentName:',name)
+   getStudentName(name,...params) {
+      console.log('App get StudentName:',name,params)
     },
-  }
+
+  },
+  mounted() {
+    // this.$refs.student.$on('atguigu',this.getStudentName)    //可以setTimeout
+    // this.$refs.student.$once('atguigu',this.getStudentName)   //触发一次
+    
+  },
 }
 </script>
 
