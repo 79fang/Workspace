e2f178a5dcf95d11472828d13a5421f658debb0d
79fang <laccat@qq.com>
2021-11-28 21:15:07 +0800

    v

--- vue/_19单文件组件/vue_test01/src/App.vue
+++ vue/_19单文件组件/vue_test01/src/App.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="app">
     <h1>{{msg}}</h1>
+
     <!-- 通过父组件给子组件传递函数类型的props实现:子给父传递数据 -->
     <School :getSchoolName="getSchoolName" />
 
     <!-- 通过父组件给子组件绑定一个自定义事件实现:子给父传递数据(第一种写法,使用@或v-on) -->
     <!-- //once 触发一次 -->
-    <Student @atguigu.once="getStudentName" />  
+    <!-- <Student @atguigu.once="getStudentName" />   -->
+    <Student @atguigu="getStudentName" @demo="m1" /> 
+
 
     <!-- 通过父组件给子组件绑定一个自定义事件实现:子给父传递数据(第二种写法,使用ref) -->
     <!-- <Student ref="student" /> -->
@@ -30,14 +33,18 @@ export default {
     getSchoolName(name){
       console.log('App 收到了 SchoolName:',name)
     },
+    //收到的第一个参数作为name,后面的参数放入params数组
    getStudentName(name,...params) {
       console.log('App get StudentName:',name,params)
     },
+    m1(){
+      console.log('demo事件被触发了')
+    }
 
   },
   mounted() {
-    // this.$refs.student.$on('atguigu',this.getStudentName)    //可以setTimeout
-    // this.$refs.student.$once('atguigu',this.getStudentName)   //触发一次
+    // this.$refs.student.$on('atguigu',this.getStudentName)    //绑定自定义事件,可以setTimeout
+    // this.$refs.student.$once('atguigu',this.getStudentName)   //绑定自定义事件(一次性),触发一次
     
   },
 }
--- vue/_19单文件组件/vue_test01/src/components/Student.vue
+++ vue/_19单文件组件/vue_test01/src/components/Student.vue
@@ -2,7 +2,11 @@
   <div class="student">
     <h2>学生姓名:{{name}}</h2>
     <h2>学生性别:{{sex}}</h2>
+    <h2>当前求和为:{{number}}</h2>
+    <button @click='add'> 点我number++</button>
     <button @click="sendStudentName">把学生名给App</button>
+    <button @click="unbind">解绑atguigu事件</button>
+    <button @click="death">销毁当前Student组件的实例(vc)</button>
 
   </div>
 </template>
@@ -14,12 +18,26 @@ export default {
     return {
       name: '张三',
       sex: '男',
+      number:0
     }
   },
   methods: {
+    add(){
+      console.log('add回调被调用了')
+      this.number++
+    },
     sendStudentName(){
       //触发Student组件实例身上的atguigu事件
-      this.$emit('atguigu',this.name)
+      this.$emit('atguigu',this.name,666,888,999)
+      this.$emit('demo')
+    },
+    unbind(){
+      // this.$off('atguigu')  //解绑一个自定义事件
+      // this.$off(['atguigu','demo']) //解绑多个自定义事件
+      this.$off() //解绑所有的自定义事件
+    },
+    death(){
+      this.$destroy() //销毁当前Student组件的实例
     }
   }
 }
